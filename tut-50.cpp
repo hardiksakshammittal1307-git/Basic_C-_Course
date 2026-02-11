b6ebcf7d0e62b48f17b15cd3adb6b75f23a92af3
Hardik Mittal <hardiksakshammittal1307@gmail.com>
2026-02-11 06:23:54 +0530

    Abstract Base Class & Pure Virtual Functions in C++

--- tut-50.cpp
+++ tut-50.cpp
@@ -26,7 +26,7 @@ public:
     }
     void display()
     {
-        cout << "This is an amazing video with title '" << title << "'" << endl;
+            cout << "This is an amazing video with title '" << title << "'" << endl;
         cout << "Ratings: " << rating << " out of 5 stars" << endl;
         cout << "Length of this video is: " << videolength << " minutes" << endl;
     }
